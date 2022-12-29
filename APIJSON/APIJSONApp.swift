f3c6cb0f74d0370fc256b2901fdd3e080f6e4a88
itsmylifestyle <hahaii00@mail.ru>
2022-12-30 01:00:13 +0600

    Initial Commit

---
+++ APIJSON/APIJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIJSONApp.swift
+//  APIJSON
+//
+//  Created by Айбек on 30.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct APIJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
